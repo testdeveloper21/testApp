b00b49b7e581a33b6ac02c206bf96b22cf1981c9
testdeveloper21 <46367282+testdeveloper21@users.noreply.github.com>
2023-01-11 18:35:56 +0530

    Update Program.cs

--- TestAppCI/Program.cs
+++ TestAppCI/Program.cs
@@ -15,7 +15,7 @@ namespace TestAppCI
         {
             CreateHostBuilder(args).Build().Run();
         }
-        tet
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
